07e7d1983b9010be079b079430bbeed4353dfcf6
euoguss <dias.gustavof@gmail.com>
2025-06-25 10:06:17 -0300

    criação do sistema de logs com session

--- app.js
+++ app.js
@@ -4,9 +4,24 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const admin = require("./routes/admin");
 const path = require("path");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const app = express();
 
+app.use(
+  session({
+    secret: "AZ6mkdVccRRQQbrPlMD1lV2JSVuML/GcXxEdjVr8VtI",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.engine(
